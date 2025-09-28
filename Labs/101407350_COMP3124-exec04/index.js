c88bbde4ce44bd16f8889d05dde291b71e822d9d
Bryan Yabut <bryan.yabut@outlook.com>
2025-09-28 01:11:33 -0400

    Exercise04 added

---
+++ Labs/101407350_COMP3124-exec04/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+app.use(express.json());
+
+
+app.use(express.static('public'));
+
+
+app.get('/hello', (req, res) => {
+  res.send('Hello Express JS');
+});
+
+
+app.get('/user', (req, res) => {
+  const firstname = req.query.firstname || 'Pritesh';
+  const lastname = req.query.lastname || 'Patel';
+  res.json({ firstname, lastname });
+});
+
+app.post('/user/:firstname/:lastname', (req, res) => {
+  const { firstname, lastname } = req.params;
+  res.json({ firstname, lastname });
+});
+
+
+app.post('/users', (req, res) => {
+  const users = req.body.users;
+  res.json(users);
+});
+
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
