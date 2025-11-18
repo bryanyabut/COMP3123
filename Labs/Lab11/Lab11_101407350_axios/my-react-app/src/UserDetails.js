0650b67d80cea64b002fffa8ed4c6eeffd99b8c6
Bryan Yabut <bryan.yabut@outlook.com>
2025-11-17 22:45:32 -0500

    lab exercise 11 added

--- Labs/Lab11/Lab11_101407350_axios/my-react-app/src/UserDetails.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function UserDetails(props) {
-    return (
-        <div>
-            <h4 key={props.user.id}>{props.user.name}</h4>
-        </div>
-    )
-}
